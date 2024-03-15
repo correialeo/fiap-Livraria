f37b9350457b4990f36097f7facee37288e30ae7
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-15 08:54:23 -0300

    Initial commit

---
+++ src/LivroService.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LivroService {
+    Livros entity = new Livros();
+    List<Livros> livros = new ArrayList<>();
+    Scanner leitor = new Scanner(System.in);
+
+    public void cadastrarLivro(){
+        System.out.println("Qual o nome do livro que deseja registrar?");
+        String livro = leitor.nextLine();
+        System.out.println("Qual o autor do livro?");
+        String autor = leitor.nextLine();
+        System.out.println("Qual o resumo do livro?");
+        String resumo = leitor.nextLine();
+        System.out.println("Qual o valor desse livro?");
+        Double valor = leitor.nextDouble();
+        livros.add(new Livros(livro, autor, resumo, valor));
+    }
+
+    public void removeLivro(){
+        System.out.println("Qual o nome do livro que você deseja remover?");
+        String nomeARemover = leitor.nextLine();
+        livros.removeIf(x -> x.getNome().equals(nomeARemover));
+    }
+
+    public void mostrarLivros(){
+        livros.forEach(x-> System.out.println(x.getNome() + "\n"+ x.getAutor()
+                + "\n"+ x.getResumo() + "\n" + x.getValor()));
+    }
+}
---
+++ src/Livros.java
@@ -0,0 +1,49 @@
+public class Livros {
+    private String nome;
+    private String autor;
+    private Double valor;
+    private String resumo;
+
+    public Livros(){
+
+    }
+
+    public Livros(String nome, String autor, String resumo, Double valor) {
+        this.nome = nome;
+        this.autor = autor;
+        this.valor = valor;
+        this.resumo = resumo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+}
